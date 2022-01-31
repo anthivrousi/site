bf1169649032d1925a4b007fde692cb6dea4193c
Anthi <anthi@orfium.com>
2022-02-01 01:34:43 +0200

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var attempt = 3;
 function validate() {
   var userUsername = document.getElementById("username").value;
   var userPassword = document.getElementById("password").value;
@@ -6,9 +7,18 @@ function validate() {
     //   "You enter wrong credentials");
     window.location.href = "index2.html";
   } else {
+    attempt--;
+    alert("You have left " + attempt + " attempt;");
     document.getElementById("wrongcredentials").innerHTML =
       "You Entered Wrong Credentials";
     document.getElementById("wrongcredentials").classList.add("redstyle");
+  }
+
+  if (attempt == 0) {
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("submit").disabled = true;
+    return false;
   }
 
   if (userUsername.length < 5) {
@@ -17,3 +27,16 @@ function validate() {
     document.getElementById("morecharacters").classList.add("redstyle");
   }
 }
+
+var modal = document.getElementById("firstbutton");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// function Reset() {
+//   document.getElementById("myform").reset();
+// }
