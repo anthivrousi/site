686d6a3d374aecdbc5c402f1a7d131460d7445ef
Anthi <anthi@orfium.com>
2022-01-31 19:32:58 +0200

    change

---
+++ index.js
@@ -0,0 +1,20 @@
+function validate() {
+  var userUsername = document.getElementById("username").value;
+  var userPassword = document.getElementById("password").value;
+  if (userUsername == "iconplatforms" && userPassword == "P@s$w0rD") {
+    // var element = (document.getElementById("wrong").innerHTML =
+    //   "You enter wrong credentials");
+    window.location.href = "index2.html";
+  } else {
+    alert("invalid");
+  }
+  //   else {
+  //     var element = (document.getElementById("morecharacters").innerHTML =
+  //       "Add More");
+  //   }
+
+  if (userUsername.length < 5) {
+    document.getElementById("morecharacters").innerHTML = "Add More";
+    document.getElementById("morecharacters").classList.add("redstyle");
+  }
+}
